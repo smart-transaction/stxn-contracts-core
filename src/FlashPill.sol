46aa2ff7188ff26698d774f48b81d4733b1cc00c
Claudia Richoux <c@laudiacay.cool>
2023-08-07 23:56:27 -0400

    starting flashpill

---
+++ src/FlashPill.sol
@@ -0,0 +1,12 @@
+// on calling the transfer function
+// you need to ensure that in the future of this transaction, they transfer the tokens back to the contract and end up being at zero.
+// ok. here we go!
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./IERC20.sol";
+
+contract FlashPill is IERC20 {
+
+}
