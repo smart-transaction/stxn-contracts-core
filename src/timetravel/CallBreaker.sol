b706a3e57c0ccb177bd176988cd31e1b76705f51
Xiangan He <xiangan9@gmail.com>
2023-11-07 14:51:37 -0500

    beginning additions to index checks

--- src/timetravel/CallBreaker.sol
+++ src/timetravel/CallBreaker.sol
@@ -93,7 +93,8 @@ contract CallBreaker is CallBreakerStorage {
     /// NOTE: Expect calls to arrive with non-null msg.data
     /// NOTE: Calldata bytes are structured as a CallObject
     fallback(bytes calldata input) external payable returns (bytes memory) {
-        return this.enterPortal(input);
+        (bytes memory portalInput, uint256 callIndex) = abi.decode(input, (bytes, uint256));
+        return this.enterPortal(portalInput, callIndex);
     }
 
     /// @notice Fetches the value associated with a given key from the associatedDataStore
@@ -116,106 +117,53 @@ contract CallBreaker is CallBreakerStorage {
         return keccak256(abi.encode(callObj, returnObj));
     }
 
-    // verify
-    // a .  11 (user doing some logic)
-    // b .  22 (solver doing some bs)
-    // c .  33 (user doing some asserts)
-    // d .  44 (solver doing more bs)
-
-    // EXAMPLE: user's desires:
-    // wants to allow: a d b c (arbitrary shit between a and c)
-    //     this is fine:
-    //     a pops c
-    //     db handle popping themselves
-    //     c pops a
-    // wants to allow: a b c d (arbitrary backrun)
-    //     this is *not* currently fine:
-    //     a pops c
-    //     b pops itself
-    //     what pops d? something before a must pop d. what goes before a? that's an illegal frontrun.
-    //     a cannot pop d because a can't know about d because d is arbitrary code provided at solve-time
-    //     c pops a
-    // want to prevent: c d b a: c before a (tricking the timeturner)
-    //     c pops a
-    //     db pop themselves
-    //     a pops c
-    //     this can be checked by setting a bool in a, and then checking and unsetting it in c- so we're okay in the current paradigm, although it is sucky and ugly
-    // wants to prevent: e a d b c (no frontrunning pls)
-    //     you can't prevent this without indices:
-    //     c pops a
-    //     add some (legal) backrun, g, after c
-    //     g pops e (e also pops g)
-    //     everything works :| unfortunately
-    //     this is fixed by adding indices :)
-    // want to prevent: a b d e (a ever being run without its call to c, which enforces invariants)
-    //     this is fixed with the timeturner- if a enterportals on c, c has to be called in verify, otherwise everything reverts
-
-    // in a, we want to see c executed at some point
-    // in a, we want nothing before a
-    // in a, call enterportal(c)
-    // in c, call enterportal(a)
-
-    // proposal: provide an index from the front, an index from the back, or a "hintdex"- the user wants to know the location of certain calls at solvetime, the solver provides that?
-    // look up return value and check into enterportal by index
-    // have a utility function that converts reverse indices into forward indices
-    // put assertions on relative ordering into userspace code! say a < c explicitly.
-    // same call twice, two different returns- how to disambiguate? indices and comparisons!
-    // need to be able to say that 
-
-    // i call enterportal with a, index 1
-    // enterportal checks that a is at index 1, returns the return value of a back to itself
-    // i call enterportal with c, index when_was_it_called(c) <- this is just a hashtable lookup in associateddata (or just another hashmap)
-    // enterportal checks that c is at index when_was_it_called(c), returns the return value of c back to itself
-    // verify handles actually making sure c is called at that index
-    // check 1 < when_was_it_called(c)
-    // xiangan's example: ind(a) + 2 == when_was_it_called(c)
-
-    // b and d need to handle their own enterportal and index calls- this is fine.
-    // no frontruns allowed- a is always 1
-    // backruns are fine
-    // arbitrary code is fine in between a and c and after c, it just needs to pop itself off the stack with knowledge of indices
-    // reshuffling a and c is no longer okay- a must be called before c, checked in a
-
-    // about hintdices:
-    // a will say ind(c) = associateddatafetch("whenwascccalled")
-    // a will assert ind(c) > 1 (1 is ind(a), a knows this, because c is checking it already through the callbreaker)
-    // enterportal(a, 1) will check in verify's call that a actually gets executed at index(1)
-    // enterportal(c, ind(c)) will check also in enterportal + verify's call using verify bookkeeping that c is executed at ind(c)
-    // it's fine that the solver is providing this value- a is checking the relative value to other calls, and enterportal is checking that the call happened there.
-
     /// @notice Executes a call and returns a value from the record of return values.
     /// @dev This function also does some accounting to track the occurrence of a given pair of call and return values.
     /// It is called as reentrancy in order to balance the calls of the solution and make things validate.
     /// @param input The call to be executed, structured as a CallObject.
+    /// @param callIndex The index of the callObj used to enforce call order.
     /// @return The return value from the record of return values.
-    function enterPortal(bytes calldata input) external payable onlyPortalOpen returns (bytes memory) {
+    function enterPortal(bytes calldata input, uint256 callIndex) external payable onlyPortalOpen returns (bytes memory) {
         // Ensure there's at least one return value available
         if (returnStore.length == 0) {
             revert OutOfReturnValues();
         }
 
-        ReturnObjectWithIndex memory lastReturn = popLastReturn();
+        ReturnObjectWithIndex memory lastReturn = _popLastReturn();
 
-        // Decode the input to obtain the CallObject and calculate a unique ID representing the call-return pair
+        // Decode the input to obtain the CallObject
         CallObjectWithIndex memory callObjWithIndex = abi.decode(input, (CallObjectWithIndex));
+        // Ensure that the index is consistent with the intended callIndex given at solve-time
+        _ensureIndexConsistency(callObjWithIndex.index, callIndex);
 
         // Check that the index of the callObj matches the index of the returnObj
         if (callObjWithIndex.index != lastReturn.index) {
             revert IndexMismatch(callObjWithIndex.index, lastReturn.index);
         }
+
+        // Calculate a unique ID representing the call-return pair
         bytes32 pairID = getCallReturnID(callObjWithIndex.callObj, lastReturn.returnObj);
 
         // Update or initialize the balance of the call-return pair
-        incrementCallBalance(pairID);
+        _incrementCallBalance(pairID);
 
         emit EnterPortal(
-            callObjWithIndex.callObj,
+            lastReturn.index,
             lastReturn.returnObj,
             pairID,
             callbalanceStore[pairID].balance,
             callObjWithIndex.index
         );
         return lastReturn.returnObj.returnvalue;
+    }
+
+    /// @dev Ensures that the index of the callObj matches the expected index
+    /// @param callIndex The index of the callObj
+    /// @param expectedIndex The expected index
+    function _ensureIndexConsistency(uint256 callIndex, uint256 expectedIndex) internal pure {
+        if (callIndex != expectedIndex) {
+            revert IndexMismatch(callIndex, expectedIndex);
+        }
     }
 
     /// @notice Verifies that the given calls, when executed, gives the correct return values
@@ -233,22 +181,22 @@ contract CallBreaker is CallBreakerStorage {
         }
 
         CallObject[] memory calls = abi.decode(callsBytes, (CallObject[]));
-        ReturnObject[] memory return_s = abi.decode(returnsBytes, (ReturnObject[]));
+        ReturnObject[] memory returnValues = abi.decode(returnsBytes, (ReturnObject[]));
 
-        if (calls.length != return_s.length) {
+        if (calls.length != returnValues.length) {
             revert LengthMismatch();
         }
 
-        resetReturnStoreWith(return_s);
-        populateAssociatedDataStore(associatedData);
+        _resetReturnStoreWith(returnValues);
+        _populateAssociatedDataStore(associatedData);
 
         for (uint256 i = 0; i < calls.length; i++) {
-            executeAndVerifyCall(calls[i]);
+            _executeAndVerifyCall(calls[i]); 
         }
 
-        ensureAllPairsAreBalanced();
+        _ensureAllPairsAreBalanced();
 
-        cleanUpStorage();
+        _cleanUpStorage();
 
         // Transfer remaining ETH balance to the block builder
         address payable blockBuilder = payable(block.coinbase);
@@ -257,18 +205,18 @@ contract CallBreaker is CallBreakerStorage {
     }
 
     /// @dev Resets the returnStore with the given ReturnObject array
-    /// @param return_s The array of ReturnObject to reset the returnStore with
-    function resetReturnStoreWith(ReturnObject[] memory return_s) internal {
+    /// @param returnValues The array of ReturnObject to reset the returnStore with
+    function _resetReturnStoreWith(ReturnObject[] memory returnValues) internal {
         delete returnStore;
-        for (uint256 i = 0; i < return_s.length; i++) {
-            ReturnObjectWithIndex memory returnObjWithIndex = ReturnObjectWithIndex({returnObj: return_s[i], index: i});
+        for (uint256 i = 0; i < returnValues.length; i++) {
+            ReturnObjectWithIndex memory returnObjWithIndex = ReturnObjectWithIndex({returnObj: returnValues[i], index: i});
             returnStore.push(returnObjWithIndex);
         }
     }
 
     /// @dev Executes a single call and verifies the result by generating the call-return pair ID
     /// @param callObj The CallObject to be executed and verified
-    function executeAndVerifyCall(CallObject memory callObj) internal {
+    function _executeAndVerifyCall(CallObject memory callObj) internal {
         if (callObj.amount > address(this).balance) {
             revert OutOfEther();
         }
@@ -280,11 +228,11 @@ contract CallBreaker is CallBreakerStorage {
         }
 
         bytes32 pairID = getCallReturnID(callObj, ReturnObject(returnvalue));
-        decrementCallBalance(pairID);
+        _decrementCallBalance(pairID);
     }
 
     /// @dev Cleans up storage by resetting returnStore and callbalanceKeyList
-    function cleanUpStorage() internal {
+    function _cleanUpStorage() internal {
         delete returnStore;
         delete callbalanceKeyList;
         for (uint256 i = 0; i < associatedDataKeyList.length; i++) {
@@ -294,7 +242,7 @@ contract CallBreaker is CallBreakerStorage {
     }
 
     // @dev Helper function to fetch and remove the last ReturnObject from the storage
-    function popLastReturn() internal returns (ReturnObjectWithIndex memory) {
+    function _popLastReturn() internal returns (ReturnObjectWithIndex memory) {
         ReturnObjectWithIndex memory lastReturn = returnStore[returnStore.length - 1];
         returnStore.pop();
         return lastReturn;
@@ -302,7 +250,7 @@ contract CallBreaker is CallBreakerStorage {
 
     /// @dev Helper function to increment the balance of a call-return pair in the storage.
     /// @param pairID The unique identifier for a call-return pair.
-    function incrementCallBalance(bytes32 pairID) internal {
+    function _incrementCallBalance(bytes32 pairID) internal {
         if (!callbalanceStore[pairID].set) {
             callbalanceStore[pairID].balance = 1;
             callbalanceKeyList.push(pairID);
@@ -314,7 +262,7 @@ contract CallBreaker is CallBreakerStorage {
 
     /// @dev Helper function to decrement the balance of a call-return pair in the storage.
     /// @param pairID The unique identifier for a call-return pair.
-    function decrementCallBalance(bytes32 pairID) internal {
+    function _decrementCallBalance(bytes32 pairID) internal {
         if (!callbalanceStore[pairID].set) {
             callbalanceStore[pairID].balance = -1;
             callbalanceKeyList.push(pairID);
@@ -327,7 +275,7 @@ contract CallBreaker is CallBreakerStorage {
     /// @notice Inserts a pair of bytes32 into the associatedDataStore and associatedDataKeyList
     /// @param key The key to be inserted into the associatedDataStore
     /// @param value The value to be associated with the key in the associatedDataStore
-    function insertIntoAssociatedDataStore(bytes32 key, bytes memory value) internal {
+    function _insertIntoAssociatedDataStore(bytes32 key, bytes memory value) internal {
         // Check if the key already exists in the associatedDataStore
         if (associatedDataStore[key].set) {
             revert KeyAlreadyExists();
@@ -344,7 +292,7 @@ contract CallBreaker is CallBreakerStorage {
 
     /// @notice Populates the associatedDataStore with a list of key-value pairs
     /// @param encodedData The abi-encoded list of (bytes32, bytes32) key-value pairs
-    function populateAssociatedDataStore(bytes memory encodedData) internal {
+    function _populateAssociatedDataStore(bytes memory encodedData) internal {
         // Decode the input data into an array of (bytes32, bytes32) pairs
         (bytes32[] memory keys, bytes[] memory values) = abi.decode(encodedData, (bytes32[], bytes[]));
 
@@ -353,16 +301,20 @@ contract CallBreaker is CallBreakerStorage {
 
         // Iterate over the keys and values arrays and insert each pair into the associatedDataStore
         for (uint256 i = 0; i < keys.length; i++) {
-            insertIntoAssociatedDataStore(keys[i], values[i]);
+            _insertIntoAssociatedDataStore(keys[i], values[i]);
         }
     }
 
     /// @dev Ensures all call-return pairs have balanced counts.
-    function ensureAllPairsAreBalanced() internal view {
+    function _ensureAllPairsAreBalanced() internal view {
         for (uint256 i = 0; i < callbalanceKeyList.length; i++) {
             if (callbalanceStore[callbalanceKeyList[i]].balance != 0) {
                 revert TimeImbalance();
             }
         }
+    }
+
+    function _getIndexFromEnd(CallObject[] memory callObjects, uint256 indexFromStart) internal pure returns (uint256) {
+        return callObjects.length - indexFromStart - 1;
     }
 }
