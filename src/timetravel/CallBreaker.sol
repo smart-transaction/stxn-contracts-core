68b41861a4f11e9ea55f3afb3434b7ce5ab99f4c
Xiangan He <xiangan9@gmail.com>
2023-11-07 14:04:30 -0500

    pr init

--- src/timetravel/CallBreaker.sol
+++ src/timetravel/CallBreaker.sol
@@ -93,7 +93,8 @@ contract CallBreaker is CallBreakerStorage {
     /// NOTE: Expect calls to arrive with non-null msg.data
     /// NOTE: Calldata bytes are structured as a CallObject
     fallback(bytes calldata input) external payable returns (bytes memory) {
-        return this.enterPortal(input);
+        (bytes memory portalInput, uint256 callIndex) = abi.decode(input, (bytes, uint256));
+        return this.enterPortal(portalInput, callIndex);
     }
 
     /// @notice Fetches the value associated with a given key from the associatedDataStore
@@ -120,14 +121,15 @@ contract CallBreaker is CallBreakerStorage {
     /// @dev This function also does some accounting to track the occurrence of a given pair of call and return values.
     /// It is called as reentrancy in order to balance the calls of the solution and make things validate.
     /// @param input The call to be executed, structured as a CallObject.
+    /// @param callIndex The index of the callObj used to enforce call order.
     /// @return The return value from the record of return values.
-    function enterPortal(bytes calldata input) external payable onlyPortalOpen returns (bytes memory) {
+    function enterPortal(bytes calldata input, uint256 callIndex) external payable onlyPortalOpen returns (bytes memory) {
         // Ensure there's at least one return value available
         if (returnStore.length == 0) {
             revert OutOfReturnValues();
         }
 
-        ReturnObjectWithIndex memory lastReturn = popLastReturn();
+        ReturnObjectWithIndex memory lastReturn = _popLastReturn();
 
         // Decode the input to obtain the CallObject and calculate a unique ID representing the call-return pair
         CallObjectWithIndex memory callObjWithIndex = abi.decode(input, (CallObjectWithIndex));
@@ -139,7 +141,7 @@ contract CallBreaker is CallBreakerStorage {
         bytes32 pairID = getCallReturnID(callObjWithIndex.callObj, lastReturn.returnObj);
 
         // Update or initialize the balance of the call-return pair
-        incrementCallBalance(pairID);
+        _incrementCallBalance(pairID);
 
         emit EnterPortal(
             callObjWithIndex.callObj,
@@ -166,22 +168,22 @@ contract CallBreaker is CallBreakerStorage {
         }
 
         CallObject[] memory calls = abi.decode(callsBytes, (CallObject[]));
-        ReturnObject[] memory return_s = abi.decode(returnsBytes, (ReturnObject[]));
+        ReturnObject[] memory returnValues = abi.decode(returnsBytes, (ReturnObject[]));
 
-        if (calls.length != return_s.length) {
+        if (calls.length != returnValues.length) {
             revert LengthMismatch();
         }
 
-        resetReturnStoreWith(return_s);
-        populateAssociatedDataStore(associatedData);
+        _resetReturnStoreWith(returnValues);
+        _populateAssociatedDataStore(associatedData);
 
         for (uint256 i = 0; i < calls.length; i++) {
-            executeAndVerifyCall(calls[i]);
+            _executeAndVerifyCall(calls[i]);
         }
 
-        ensureAllPairsAreBalanced();
+        _ensureAllPairsAreBalanced();
 
-        cleanUpStorage();
+        _cleanUpStorage();
 
         // Transfer remaining ETH balance to the block builder
         address payable blockBuilder = payable(block.coinbase);
@@ -190,18 +192,18 @@ contract CallBreaker is CallBreakerStorage {
     }
 
     /// @dev Resets the returnStore with the given ReturnObject array
-    /// @param return_s The array of ReturnObject to reset the returnStore with
-    function resetReturnStoreWith(ReturnObject[] memory return_s) internal {
+    /// @param returnValues The array of ReturnObject to reset the returnStore with
+    function _resetReturnStoreWith(ReturnObject[] memory returnValues) internal {
         delete returnStore;
-        for (uint256 i = 0; i < return_s.length; i++) {
-            ReturnObjectWithIndex memory returnObjWithIndex = ReturnObjectWithIndex({returnObj: return_s[i], index: i});
+        for (uint256 i = 0; i < returnValues.length; i++) {
+            ReturnObjectWithIndex memory returnObjWithIndex = ReturnObjectWithIndex({returnObj: returnValues[i], index: i});
             returnStore.push(returnObjWithIndex);
         }
     }
 
     /// @dev Executes a single call and verifies the result by generating the call-return pair ID
     /// @param callObj The CallObject to be executed and verified
-    function executeAndVerifyCall(CallObject memory callObj) internal {
+    function _executeAndVerifyCall(CallObject memory callObj) internal {
         if (callObj.amount > address(this).balance) {
             revert OutOfEther();
         }
@@ -213,11 +215,11 @@ contract CallBreaker is CallBreakerStorage {
         }
 
         bytes32 pairID = getCallReturnID(callObj, ReturnObject(returnvalue));
-        decrementCallBalance(pairID);
+        _decrementCallBalance(pairID);
     }
 
     /// @dev Cleans up storage by resetting returnStore and callbalanceKeyList
-    function cleanUpStorage() internal {
+    function _cleanUpStorage() internal {
         delete returnStore;
         delete callbalanceKeyList;
         for (uint256 i = 0; i < associatedDataKeyList.length; i++) {
@@ -227,7 +229,7 @@ contract CallBreaker is CallBreakerStorage {
     }
 
     // @dev Helper function to fetch and remove the last ReturnObject from the storage
-    function popLastReturn() internal returns (ReturnObjectWithIndex memory) {
+    function _popLastReturn() internal returns (ReturnObjectWithIndex memory) {
         ReturnObjectWithIndex memory lastReturn = returnStore[returnStore.length - 1];
         returnStore.pop();
         return lastReturn;
@@ -235,7 +237,7 @@ contract CallBreaker is CallBreakerStorage {
 
     /// @dev Helper function to increment the balance of a call-return pair in the storage.
     /// @param pairID The unique identifier for a call-return pair.
-    function incrementCallBalance(bytes32 pairID) internal {
+    function _incrementCallBalance(bytes32 pairID) internal {
         if (!callbalanceStore[pairID].set) {
             callbalanceStore[pairID].balance = 1;
             callbalanceKeyList.push(pairID);
@@ -247,7 +249,7 @@ contract CallBreaker is CallBreakerStorage {
 
     /// @dev Helper function to decrement the balance of a call-return pair in the storage.
     /// @param pairID The unique identifier for a call-return pair.
-    function decrementCallBalance(bytes32 pairID) internal {
+    function _decrementCallBalance(bytes32 pairID) internal {
         if (!callbalanceStore[pairID].set) {
             callbalanceStore[pairID].balance = -1;
             callbalanceKeyList.push(pairID);
@@ -260,7 +262,7 @@ contract CallBreaker is CallBreakerStorage {
     /// @notice Inserts a pair of bytes32 into the associatedDataStore and associatedDataKeyList
     /// @param key The key to be inserted into the associatedDataStore
     /// @param value The value to be associated with the key in the associatedDataStore
-    function insertIntoAssociatedDataStore(bytes32 key, bytes memory value) internal {
+    function _insertIntoAssociatedDataStore(bytes32 key, bytes memory value) internal {
         // Check if the key already exists in the associatedDataStore
         if (associatedDataStore[key].set) {
             revert KeyAlreadyExists();
@@ -277,7 +279,7 @@ contract CallBreaker is CallBreakerStorage {
 
     /// @notice Populates the associatedDataStore with a list of key-value pairs
     /// @param encodedData The abi-encoded list of (bytes32, bytes32) key-value pairs
-    function populateAssociatedDataStore(bytes memory encodedData) internal {
+    function _populateAssociatedDataStore(bytes memory encodedData) internal {
         // Decode the input data into an array of (bytes32, bytes32) pairs
         (bytes32[] memory keys, bytes[] memory values) = abi.decode(encodedData, (bytes32[], bytes[]));
 
@@ -286,16 +288,20 @@ contract CallBreaker is CallBreakerStorage {
 
         // Iterate over the keys and values arrays and insert each pair into the associatedDataStore
         for (uint256 i = 0; i < keys.length; i++) {
-            insertIntoAssociatedDataStore(keys[i], values[i]);
+            _insertIntoAssociatedDataStore(keys[i], values[i]);
         }
     }
 
     /// @dev Ensures all call-return pairs have balanced counts.
-    function ensureAllPairsAreBalanced() internal view {
+    function _ensureAllPairsAreBalanced() internal view {
         for (uint256 i = 0; i < callbalanceKeyList.length; i++) {
             if (callbalanceStore[callbalanceKeyList[i]].balance != 0) {
                 revert TimeImbalance();
             }
         }
+    }
+
+    function _getIndexFromEnd(CallObject[] memory callObjects, uint256 indexFromStart) internal pure returns (uint256) {
+        return callObjects.length - indexFromStart - 1;
     }
 }
