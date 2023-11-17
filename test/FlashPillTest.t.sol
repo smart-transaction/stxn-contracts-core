c4acbf970c05a2d6bd8c25fca74165293ab5247f
xBalbinus <xiangan9@gmail.com>
2023-11-17 13:47:20 -0500

    add back a note to complete flashpill

---
+++ test/FlashPillTest.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.6.2 <0.9.0;
+
+import "forge-std/Test.sol";
+import "../src/timetravel/CallBreaker.sol";
+import "../test/examples/FlashPill.sol";
+
+contract FlashPillTest is Test {
+    CallBreaker private callbreaker;
+    FlashPill private flashpill;
+
+    function setUp() public {
+        callbreaker = new CallBreaker();
+        flashpill = new FlashPill(address(callbreaker));
+    }
+
+    // @TODO: Complete FlashPill example
+    function test() public pure {
+        assert(true);
+    }
+}
