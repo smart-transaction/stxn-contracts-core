12ed94b8f186acbdcbca8212baa6b7590c986840
TokenTitan <r@stxn.io>
2024-08-19 15:07:49 +0530

    feat: added ownable and other utility functions to faucet

--- src/utilities/Faucet.sol
+++ src/utilities/Faucet.sol
@@ -1,54 +1,56 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.26;
 
-contract Faucet {
-    //state variable to keep track of owner and amount of ETHER to dispense
+import "openzeppelin-upgradeable/access/Ownable2StepUpgradeable.sol";
+
+contract Faucet is Ownable2StepUpgradeable {
     address public owner;
-    uint public amountAllowed = 0.5 ether;
+    uint256 public dripAmount;
+    uint256 public lockDuration;
 
+    /// @notice used to block users of faucet for 24 hours after taking every call
+    /// @dev mapping to store address and blocktime + 1 day
+    mapping(address => uint256) public lockTime;
 
-    //mapping to keep track of requested rokens
-    //Address and blocktime + 1 day is saved in TimeLock
-    mapping(address => uint) public lockTime;
+    event DripAmountUpdated(uint256 amount);
+    event LockDurationUpdated(uint256 lockDuration);
+    event FundsTransferred(address indexed requestor, uint256 dripAmount);
 
-
-    //constructor to set the owner
-    constructor() payable { 
-	owner = msg.sender;
+    function initialize() external initializer {
+        __Ownable_init(_msgSender());
+        _setDripAmount(0.5 ether);
+        lockDuration = 1 days;
     }
 
-    //function modifier
-    modifier onlyOwner {
-        require(msg.sender == owner, "Only owner can call this function.");
-        _; 
+    /// @notice call to get funds from the faucet
+    function requestFunds(address payable _requestor) external payable onlyOwner {
+        // check if funds were transferred recently
+        require(block.timestamp > lockTime[_requestor], "Faucet: Please try later");
+        // check if there is enough balance
+        require(address(this).balance > dripAmount, "Faucet: Out of funds");
+
+        _requestor.transfer(dripAmount);         
+        lockTime[_requestor] = block.timestamp + lockDuration;
+        emit FundsTransferred(_requestor, dripAmount);
     }
 
-
-    //function to change the owner.  Only the owner of the contract can call this function
-    function setOwner(address newOwner) public onlyOwner {
-        owner = newOwner;
+    /// @notice call to change the amount transferred when requests are made
+    function changeDripAmount(uint256 newDripAmount) external onlyOwner {
+        _setDripAmount(newDripAmount);
     }
 
-
-    //function to set the amount allowable to be claimed. Only the owner can call this function
-    function setAmountallowed(uint setAmountAllowed) public onlyOwner {
-        amountAllowed = setAmountAllowed;
+    /// @notice 
+    function changeLockDuration(uint256 newLockDuration) external onlyOwner {
+        require(newLockDuration > 0, "Faucet: Invalid duration value");
+        lockDuration = newLockDuration;
+        emit LockDurationUpdated(newLockDuration);
     }
 
     // function to add funds to the smart contract
-    function addFunds() public payable { }
+    function receive() external payable {}
 
-    //function to send tokens from faucet to an address
-    function requestTokens(address payable _requestor) public payable onlyOwner {
-
-        //perform a few checks to make sure function can execute
-        require(block.timestamp > lockTime[_requestor], "Lock time has not expired. Please try again later");
-        require(address(this).balance > amountAllowed, "Not enough funds in the faucet.");
-
-        //if the balance of this contract is greater then the requested amount send funds
-        _requestor.transfer(0.5 ether);        
- 
-        //updates locktime 1 day from now
-        lockTime[_requestor] = block.timestamp + 1 days;
+    function _setDripAmount(uint256 newDripAmount) private {
+        dripAmount = newDripAmount;
+        emit DripAmountUpdated(newDripAmount);
     }
 }
