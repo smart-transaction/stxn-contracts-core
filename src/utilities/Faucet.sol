6144a2a3ebd554e66ee9491cdcd2af65a6291898
TokenTitan <r@stxn.io>
2024-08-21 16:02:42 +0530

    feat: added access control to faucet

--- src/utilities/Faucet.sol
+++ src/utilities/Faucet.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.26;
 
-import "openzeppelin-upgradeable/access/Ownable2StepUpgradeable.sol";
+import "openzeppelin-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract Faucet is Ownable2StepUpgradeable {
-    address public owner;
+contract Faucet is AccessControlUpgradeable {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
     uint256 public dripAmount;
     uint256 public lockDuration;
 
@@ -15,15 +16,17 @@ contract Faucet is Ownable2StepUpgradeable {
     event DripAmountUpdated(uint256 amount);
     event LockDurationUpdated(uint256 lockDuration);
     event FundsTransferred(address indexed requestor, uint256 dripAmount);
+    event FundsGranted(address indexed receiver, uint256 amount);
 
     function initialize() external initializer {
-        __Ownable_init(_msgSender());
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _grantRole(ADMIN_ROLE, _msgSender());
         _setDripAmount(0.5 ether);
         lockDuration = 1 days;
     }
 
     /// @notice call to get funds from the faucet
-    function requestFunds(address payable _requestor) external payable onlyOwner {
+    function requestFunds(address payable _requestor) external payable onlyRole(ADMIN_ROLE) {
         // check if funds were transferred recently
         require(block.timestamp > lockTime[_requestor], "Faucet: Please try later");
         // check if there is enough balance
@@ -35,7 +38,7 @@ contract Faucet is Ownable2StepUpgradeable {
     }
 
     /// @notice admin call to grant large funds to a receiver
-    function grantFunds(address payable _receiver, uint256 _amount) external payable onlyOwner {
+    function grantFunds(address payable _receiver, uint256 _amount) external payable onlyRole(ADMIN_ROLE) {
         // check if there is enough balance
         require(address(this).balance > _amount, "Faucet: Not enough funds");
 
@@ -44,19 +47,19 @@ contract Faucet is Ownable2StepUpgradeable {
     }
 
     /// @notice call to change the amount transferred when requests are made
-    function changeDripAmount(uint256 newDripAmount) external onlyOwner {
+    function changeDripAmount(uint256 newDripAmount) external onlyRole(ADMIN_ROLE) {
         _setDripAmount(newDripAmount);
     }
 
     /// @notice 
-    function changeLockDuration(uint256 newLockDuration) external onlyOwner {
+    function changeLockDuration(uint256 newLockDuration) external onlyRole(ADMIN_ROLE) {
         require(newLockDuration > 0, "Faucet: Invalid duration value");
         lockDuration = newLockDuration;
         emit LockDurationUpdated(newLockDuration);
     }
 
     // function to add funds to the smart contract
-    function receive() external payable {}
+    receive() external payable {}
 
     function _setDripAmount(uint256 newDripAmount) private {
         dripAmount = newDripAmount;
