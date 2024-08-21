714311173ccfb007542a8e1aff191a2a657f43c0
mevtxn <nikhil@stxn.io>
2024-08-21 13:42:57 +0530

    Change signature to deployLestnet

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ read NETWORK
 if [ "$NETWORK" = "mainnet" ]; then
    SIGNATURE="deployMainnet()"
 else
-   SIGNATURE="deployTestnet(uint256)"
+   SIGNATURE="deployLestnet(uint256)"
 fi
 
 if [ "$NETWORK" = "mainnet" ]
