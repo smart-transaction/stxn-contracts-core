d2220c17304b301a18cdbc3fc2224853b8e69f1e
TokenTitan <r@stxn.io>
2024-08-28 15:34:19 +0530

    refactor: change only laminator to only owner in execute

--- src/interfaces/ICallBreaker.sol
+++ src/interfaces/ICallBreaker.sol
@@ -9,4 +9,10 @@ interface ICallBreaker {
     /// @param callObjs The call objects that were executed.
     /// @param returnObjs The return objects that were returned from the call objects.
     function verify(bytes memory callObjs, bytes memory returnObjs) external;
+
+    /* 
+     * @notice Get the portal status
+     * @return boolean value as true if portal is open
+    */ 
+    function isPortalOpen() external view returns (bool status);
 }
