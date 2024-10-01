edfe9fa334d045c3e47070cf4d1eca6301ef2328
mevtxn <nikhil@stxn.io>
2024-10-01 19:02:19 +0530

    Refactor: flash loan function signature

--- test/utils/MockFlashLoan.sol
+++ test/utils/MockFlashLoan.sol
@@ -11,38 +11,30 @@ interface IFlashLoanBorrower {
 
 /**
  * @notice Simplified mock version of Flash Loan Provider
- * @dev not to be used for anything other than local tests
+ * @dev not to be used for anything other than tests and demo
  */
 contract MockFlashLoan {
     uint256 public constant DECIMAL = 1e18;
 
-    uint256 private _balanceOfUsdt;
-    uint256 private _balanceOfDai;
-
-    IERC20 public usdt;
+    IERC20 public weth;
     IERC20 public dai;
 
-    uint256 public feePercentage = 10; // 1%
-
-    constructor(IERC20 _usdt, IERC20 _dai) {
-        usdt = _usdt;
+    constructor(IERC20 _weth, IERC20 _dai) {
+        weth = _weth;
         dai = _dai;
-
-        // let initial liquidity be 100 for both Usdt & Dai
-        _balanceOfDai = 100 * DECIMAL;
-        _balanceOfUsdt = 100 * DECIMAL;
     }
 
-    function maxFlashLoan() external view returns (uint256 usdtBalance, uint256 daiBalance) {
-        return (_balanceOfUsdt, _balanceOfDai);
+    function maxFlashLoan() external view returns (uint256, uint256) {
+        return (weth.balanceOf(address(this)), dai.balanceOf(address(this)));
     }
 
-    function flashFee(address tokenAddress, uint256 amount) public view returns (uint256) {
-        require(tokenAddress == address(usdt) || tokenAddress == address(dai), "Unsupported token");
-        return (amount * feePercentage) / 1000; // Fee as 1% of the loaned amount
-    }
-
-    function flashLoan(address receiver, uint256 usdtAmount, uint256 daiAmount) external returns (bool) {
+    function flashLoan(
+        address receiver,
+        uint256 usdtAmount,
+        bytes calldata usdtData,
+        uint256 daiAmount,
+        bytes calldata daiData
+    ) external returns (bool) {
         uint256 usdtFee = flashFee(address(usdt), _balanceOfUsdt);
         uint256 daiFee = flashFee(address(dai), _balanceOfDai);
 
@@ -50,15 +42,15 @@ contract MockFlashLoan {
         require(daiAmount <= _balanceOfDai, "Insufficient dai liquidity");
 
         // Transfer tokens to the borrower
-        usdt.transfer(receiver, usdtAmount);
-        dai.transfer(receiver, daiAmount);
+        weth.transfer(receiver, amountA);
+        dai.transfer(receiver, amountB);
 
         // Call the borrower's onFlashLoan function once (consolidated)
         IFlashLoanBorrower(receiver).onFlashLoan(msg.sender, address(usdt), usdtAmount, address(dai), daiAmount);
 
         // Fetch the amount + fee via transferFrom
-        usdt.transferFrom(receiver, address(this), usdtAmount + usdtFee);
-        dai.transferFrom(receiver, address(this), daiAmount + daiFee);
+        weth.transferFrom(receiver, address(this), amountA);
+        dai.transferFrom(receiver, address(this), amountB);
 
         return true;
     }
