92b4c676f3702c0dd16653ff0c4302ebba2c5bf8
mevtxn <nikhil@stxn.io>
2024-12-11 19:20:23 +0530

    chore: add batchMint function to call TimeToken contract for minting tokens

--- test/examples/BlockTime/BlockTimeScheduler.sol
+++ test/examples/BlockTime/BlockTimeScheduler.sol
@@ -11,6 +11,10 @@ interface IBlockTime {
     function moveTime(BlockTime.Chronicle[] calldata chronicles, uint256 meanCurrentEarthTime) external;
 }
 
+interface ITimeToken {
+    function batchMint(address[] memory to, uint256[] memory amounts) public;
+}
+
 /**
  * @notice This is an POC example of a block scheduler
  */
@@ -21,17 +25,25 @@ contract BlockTimeScheduler is SmarterContract, Ownable {
         uint256 meanCurrentErathTime;
     }
 
+    struct BatchMintData {
+        address[] receiver;
+        uint256[] amounts;
+    }
+
     bool public shouldContinue;
     address public callBreaker;
+    ITimeToken public timeToken;
     IBlockTime public blockTime;
 
-    constructor(address _callBreaker, address _blockTime, address _owner) SmarterContract(_callBreaker) Ownable(_owner) {
+    constructor(address _callBreaker, address _blockTime, address _timeToken, address _owner) SmarterContract(_callBreaker) Ownable(_owner) {
         callBreaker = _callBreaker;
         blockTime = IBlockTime(_blockTime);
+        timeToken = ITimeToken(_timeToken);
         shouldContinue = true;
     }
 
-    function updateTime() external {
+    /// @dev The onlyOwner modifier will be later changed to execute calls through a governance proposal
+    function updateTime() external onlyOwner {
         bytes32 key = keccak256(abi.encodePacked("MoveTimeData"));
         bytes memory data = CallBreaker(payable(callBreaker)).fetchFromAssociatedDataStore(key);
 
@@ -48,12 +60,31 @@ contract BlockTimeScheduler is SmarterContract, Ownable {
         assertFutureCallTo(callObj, 1);
     }
 
+    /// @dev The onlyOwner modifier will be later changed to execute calls through a governance proposal
+    function  batchMint()  external onlyOwner {
+        bytes32 key = keccak256(abi.encodePacked("BatchMintData"));
+        bytes memory data = CallBreaker(payable(callBreaker)).fetchFromAssociatedDataStore(key);
+
+        BatchMintData memory batchMintData = abi.decode(data, (BatchMintData));
+        timeToken.batchMint(batchMintData.receiver, batchMintData.amounts);
+
+        CallObject memory callObj = CallObject({
+            amount: 0,
+            addr: address(this),
+            gas: 1000000,
+            callvalue: abi.encodeWithSignature("verifySignature(bytes)", data)
+        });
+
+        assertFutureCallTo(callObj, 1);
+    }
+
     /// @notice function to be checked by Laminator before rescheduling a call to disburseKITNs
+    /// @dev The onlyOwner modifier will be later changed to execute calls through a governance proposal
     function setContinue(bool _shouldContinue) external onlyOwner {
         shouldContinue = _shouldContinue;
     }
 
-        /// @notice function to be called by solver to ensure a succesful and valid call
+    /// @notice function to be called by solver to ensure a succesful and valid call
     function verifySignature(bytes calldata /* data */ ) public view {
         bytes32 key = keccak256(abi.encodePacked("CleanAppSignature"));
         bytes memory signature = CallBreaker(payable(callBreaker)).fetchFromAssociatedDataStore(key);
